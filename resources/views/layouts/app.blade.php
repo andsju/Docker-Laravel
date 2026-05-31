18a67935ebf58755d2a650eef091b43bf83671ef
Anders Sjunnesson <anders.sj@glimnet.se>
2026-05-31 21:15:01 +0200

    folder structure check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel')</title>
     <link rel="stylesheet" href="/css/app.css">
-    @vite('resources/scss/app.scss')
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <nav>
